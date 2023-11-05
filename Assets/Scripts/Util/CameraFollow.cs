a119c4bf465965acef900ab85e6028289a101dfb
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-11-05 18:56:28 -0400

    Added basic camera following player system

---
+++ Assets/Scripts/Util/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + new Vector3(0, 0, transform.position.z);
+            
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            
+            transform.position = smoothedPosition;
+        }
+        
+    }
+}
