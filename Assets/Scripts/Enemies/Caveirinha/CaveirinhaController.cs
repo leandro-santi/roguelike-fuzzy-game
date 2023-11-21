bf1f3e66eb76091b5b956b4ccd2ec71030190b1b
Leandro Santiago <lsds.snf20@uea.edu.br>
2023-11-21 17:33:15 -0400

    Updating enemy "caveirinha" controller

--- Assets/Scripts/Enemies/Caveirinha/CaveirinhaController.cs
+++ Assets/Scripts/Enemies/Caveirinha/CaveirinhaController.cs
@@ -15,17 +15,21 @@ public class CaveirinhaController : MonoBehaviour
     private EnemyState _currentState;
 
     [Header("Walk")] public float walkSpeed = 2f;
-    public float chaseSpeed = 5f;
+
+    [Header("Chase")] public float chaseSpeed = 5f;
+    public float chaseRange;
 
     [Header("Attack")] public float attackRange = 2f;
     public float attackCooldown = 2f;
 
     private Transform _player;
     private float _nextAttackTime = 0f;
+    private Vector2 _randomDirection;
+    private float _walkTimer = 0f;
 
     void Start()
     {
-        _currentState = EnemyState.Idle;
+        _currentState = EnemyState.Walk;
         _player = GameObject.FindGameObjectWithTag("Player")
             .transform;
     }
@@ -51,63 +55,83 @@ public class CaveirinhaController : MonoBehaviour
 
     void Idle()
     {
-        // Debug.Log("Idle");
+        // int randomMove = Random.Range(0, 2);
+        //
+        // if (randomMove == 0)
+        // {
+        //     Walk();
+        // }
+        //
+        // else
+        // {
+        //     Idle();
+        // }
 
-        if (Time.time > _nextAttackTime)
-        {
-            _currentState = EnemyState.Walk;
-        }
+        // Walk();
+        // _currentState = EnemyState.Walk;
     }
 
     void Walk()
     {
         // Debug.Log("Walk");
 
+        _walkTimer += Time.deltaTime;
+        
+        while (_walkTimer >= 3f)
+        {
+            // transform.Translate(Vector2.right * (walkSpeed * Time.deltaTime * 1000));
+            // _randomDirection = Random.insideUnitCircle.normalized;
+            // transform.Translate(_randomDirection * (walkSpeed * Time.deltaTime));
+            // _walkTimer = 0f;
+            _currentState = EnemyState.Idle;
+            _walkTimer = 0;
+        }
+
         transform.Translate(Vector2.right * (walkSpeed * Time.deltaTime));
 
-        if (Vector2.Distance(transform.position, _player.position) < attackRange)
-        {
-            _currentState = EnemyState.Chase;
-        }
+        // if (Vector2.Distance(transform.position, _player.position) < chaseRange)
+        // {
+        //     _currentState = EnemyState.Chase;
+        // }
     }
 
     void Chase()
     {
-        Debug.Log("Chase");
+        // Debug.Log("Chase");
 
-        transform.right = _player.position - transform.position;
-
+        // transform.right = _player.position - transform.position;
+        //
         // transform.Translate(Vector2.right * (chaseSpeed * Time.deltaTime));
-
-        if (Vector2.Distance(transform.position, _player.position) < attackRange)
-        {
-            _currentState = EnemyState.Attack;
-        }
-
-        else if (Vector2.Distance(transform.position, _player.position) > attackRange * 2)
-        {
-            _currentState = EnemyState.Walk;
-        }
+        //
+        // if (Vector2.Distance(transform.position, _player.position) < attackRange)
+        // {
+        //     _currentState = EnemyState.Attack;
+        // }
+        //
+        // else if (Vector2.Distance(transform.position, _player.position) > attackRange * 2)
+        // {
+        //     _currentState = EnemyState.Walk;
+        // }
     }
 
     void Attack()
     {
-        Debug.Log("Attack");
+        // Debug.Log("Attack");
 
-        int randomAttack = Random.Range(0, 2);
-
-        if (randomAttack == 0)
-        {
-            MeleeAttack();
-        }
-
-        else
-        {
-            RangedAttack();
-        }
-
-        _currentState = EnemyState.Idle;
-        _nextAttackTime = Time.time + attackCooldown;
+        // int randomAttack = Random.Range(0, 2);
+        //
+        // if (randomAttack == 0)
+        // {
+        //     MeleeAttack();
+        // }
+        //
+        // else
+        // {
+        //     RangedAttack();
+        // }
+        //
+        // _currentState = EnemyState.Idle;
+        // _nextAttackTime = Time.time + attackCooldown;
     }
 
     void MeleeAttack()
