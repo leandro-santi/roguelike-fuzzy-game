fa81a36315d972195ee0d1c921a35a483641ceb4
Leandro Santiago <leandro.s4nt1@gmail.com>
2024-01-03 17:35:24 -0400

    Created knockback feedback on enemy

--- Assets/Scripts/Enemies/General/EnemyHealth.cs
+++ Assets/Scripts/Enemies/General/EnemyHealth.cs
@@ -7,9 +7,32 @@ public class EnemyHealth : MonoBehaviour
     public int maxHealth = 3;
     private int _currentHealth;
 
+    public float knockbackForce = 2f;
+    public float knockbackDuration = 0.5f;
+    private bool _isKnockedBack = false;
+    private float _knockbackTimer;
+
+    private Rigidbody2D _rigidbody;
+
     void Start()
     {
         _currentHealth = maxHealth;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (_isKnockedBack)
+        {
+            _knockbackTimer -= Time.deltaTime;
+
+            if (_knockbackTimer <= 0f)
+            {
+                _isKnockedBack = false;
+                _rigidbody.velocity = Vector2.zero;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -17,6 +40,7 @@ public class EnemyHealth : MonoBehaviour
         if (other.CompareTag("PlayerSword"))
         {
             TakeDamage(1);
+            ApplyKnockback(other.transform.position);
         }
     }
 
@@ -27,6 +51,17 @@ public class EnemyHealth : MonoBehaviour
         if (_currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    void ApplyKnockback(Vector3 swordPosition)
+    {
+        if (_rigidbody != null && !_isKnockedBack)
+        {
+            Vector2 knockbackDirection = (transform.position - swordPosition).normalized;
+            _rigidbody.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+            _isKnockedBack = true;
+            _knockbackTimer = knockbackDuration;
         }
     }
 
